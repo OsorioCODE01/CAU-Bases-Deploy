cba49565c886a40dade7794b45234cc3b3de4b9a
OsorioCODE01 <nicolas.osor.gal@gmail.com>
2024-10-01 21:46:40 -0500

    Seccion 5 6/21

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CounterModule } from './counter/counter.module';
-import { HeroesModule } from './heroes/heroes.module';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
